6465b52a18d73506a8f3d7d080e6885e34b63006
Gkiokan Sali <gkiokan@hotmail.de>
2016-12-29 01:37:08 +0100

    Migration created...

--- Database/Migrations/2016_12_28_220137_create_company_table.php
+++ Database/Migrations/2016_12_28_220137_create_company_table.php
@@ -14,6 +14,22 @@ class CreateCompanyTable extends Migration
     {
         Schema::create('company', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+
+            $table->string('name')->nullable()->default('');
+            $table->string('hr')->nullable()->default('');
+            $table->string('owner')->nullable()->default('');
+
+            $table->string('street')->nullable()->default('');
+            $table->string('plz')->nullable()->default('');
+            $table->string('city')->nullable()->default('');
+            $table->string('state')->nullable()->default('');
+            $table->string('country')->nullable()->default('');
+
+            $table->string('tel')->nullable()->default('');
+            $table->string('fax')->nullable()->default('');
+            $table->string('email_info')->nullable()->default('');
+            $table->string('email_support')->nullable()->default('');
 
             $table->timestamps();
         });
